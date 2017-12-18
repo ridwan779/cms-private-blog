c89489f835897e5fb855afad4ff87845bb22b715
Ridwan Alamsyah <ridwanalamsyah96@gmail.com>
2017-12-18 11:42:26 +0700

    First Commit

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+	<title>Rikatsu | Under Development</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/materialize/css/materialize.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/font-awesome/css/font-awesome.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/layout.css') }}">
+</head>
+<body>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="left brand-logo">LOGO</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">Menu 1</a></li>
+        <li><a href="#">Menu 2</a></li>
+        <li><a href="#">Menu 3</a></li>
+        <li><a href="#">Menu 4</a></li>
+        <li><a href="#">Menu 5</a></li>
+        <li><a href="#">Menu 6</a></li>
+        <li><a href="#">About Us</a></li>
+        <li id="nav-search">
+          <a href="#" data-toggle="search-dropdown"><i class="fa fa-search fa-fw" aria-hidden="true"></i></a>
+          <div class="search-dropdown">
+            <form>
+              <div class="input-group">
+                <input type="text" name="search" class="form-input" placeholder="Search...">
+                <div class="group">
+                  <button type="submit" class="btn-transparent"><i class="fa fa-search"></i></button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="header">
+  	 <div class="row">
+    		<div class="article-header">
+      		 <div class="article-wrapper l-article">
+      				<div class="article-content">
+                <a href="#" class="link-article"></a>
+                <div class="cover">
+      					 <img src="{{ asset('assets/images/content/megadimension_neptunia_vii.jpg') }}">
+                </div>
+                <div class="meta">
+                  <a href="#" class="link-article"></a>
+                  <a href="#" class="category-l">NEPTUNIA</a>
+                  <div class="title-l">
+                    <a href="title-l">Series Game Neptunia Akan Mendapatkan Game Baru Jenis VR</a>
+                  </div>
+                </div>
+      				</div>
+      			</div>
+            <div class="article-wrapper-s">
+              <div class="s-article">
+                <div class="article-content">
+                  <a href="#" class="link-article"></a>
+                  <div class="cover">
+                    <img src="{{ asset('assets/images/content/atelier_lydie_suelle.jpg') }}">
+                  </div>
+                  <div class="meta">
+                    <a href="#" class="link-article"></a>
+                    <a href="test-c" class="category">ATELIER TRILOGY</a>
+                    <div class="title">
+                      <a href="test">Atelier Lydie & Suelle Mengeluarkan PV Baru</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="s-article">
+                <div class="article-content">
+                  <a href="#" class="link-article"></a>
+                  <div class="cover">
+                    <img src="{{ asset('assets/images/content/bang_dream.png') }}">
+                  </div>
+                  <div class="meta">
+                    <a href="#" class="link-article"></a>
+                    <a href="test" class="category">BANG DREAM!</a>
+                    <div class="title">
+                      <a href="#">BanG Dream! Masuk 5 Besar Best Game 2017 Playstore Region Jepang</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="article-wrapper-s">
+              <div class="s-article">
+                <div class="article-content">
+                  <a href="#" class="link-article"></a>
+                  <div class="cover">
+                    <img src="{{ asset('assets/images/content/nights-of-azure.jpg') }}">
+                  </div>
+                  <div class="meta">
+                    <a href="#" class="link-article"></a>
+                    <a href="#" class="category">NIGHT OF AZURE</a>
+                    <div class="title">
+                      <a href="#">Nights of Azure Game RPG Dengan Bumbu "Yuri" Di Dalamnya</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="s-article">
+                <div class="article-content">
+                  <a href="#" class="link-article"></a>
+                  <div class="cover">
+                    <img src="{{ asset('assets/images/content/milishita.jpg') }}">
+                  </div>
+                  <div class="meta">
+                    <a href="#" class="link-article"></a>
+                    <a href="#" class="category">IDOLMASTER</a>
+                    <div class="title">
+                      <a href="#">Capai Target Pre-Register, iDOLMASTER Million Live - Theater Days Akan Membagikan Ticket SSR</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+           </div>
+    		</div>
+  	 </div>
+  </div>
+  <div class="content-wrapper">
+      <h3>Recent Post</h3>
+      <div class="post-wrapper">
+          <div class="row">
+              <div class="col s12 m6 l8">
+                  <div class="content-title"><a href="#">GAME</a></div>
+                  <div class="article-wrapper">
+                    <a href="atelier-lydie-suelle" class="link-article"></a>
+                    <div class="img-article">
+                      <img src="{{ asset('assets/images/content/atelier_lydie_suelle.jpg') }}">
+                    </div>
+                    <div class="meta">
+                      <a href="#" class="link-article"></a>
+                      <div class="article-title">
+                        Atelier Lydie & Suelle Mengeluarkan PV Baru
+                      </div>
+                      <div class="article-date">
+                        <span>20 OKTOBER 2017</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="article-wrapper">
+                    <a href="megadimension_neptunia_vii" class="link-article"></a>
+                    <div class="img-article">
+                      <img src="{{ asset('assets/images/content/megadimension_neptunia_vii.jpg') }}">
+                    </div>
+                    <div class="meta">
+                      <a href="#" class="link-article"></a>
+                      <div class="article-title">
+                        Series Game Neptunia Akan Mendapatkan Game Baru Jenis VR
+                      </div>
+                      <div class="article-date">
+                        <span>20 OKTOBER 2017</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="article-wrapper">
+                    <a href="milishita" class="link-article"></a>
+                    <div class="img-article">
+                      <img src="{{ asset('assets/images/content/milishita.jpg') }}">
+                    </div>
+                    <div class="meta">
+                      <a href="#" class="link-article"></a>
+                      <div class="article-title">
+                        Capai Target Pre-Register, iDOLMASTER Million Live - Theater Days Akan Membagikan Ticket SSR Secara Gratis!
+                      </div>
+                      <div class="article-date">
+                        <span>20 OKTOBER 2017</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="article-wrapper">
+                    <a href="nights-of-azure" class="link-article"></a>
+                    <div class="img-article">
+                      <img src="{{ asset('assets/images/content/nights-of-azure.jpg') }}">
+                    </div>
+                    <div class="meta">
+                      <a href="#" class="link-article"></a>
+                      <div class="article-title">
+                        Nights of Azure Game RPG Dengan Bumbu "Yuri" Di Dalamnya
+                      </div>
+                      <div class="article-date">
+                        <span>20 OKTOBER 2017</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="article-wrapper">
+                    <a href="nights-of-azure" class="link-article"></a>
+                    <div class="img-article">
+                      <img src="{{ asset('assets/images/content/nights-of-azure.jpg') }}">
+                    </div>
+                    <div class="meta">
+                      <a href="#" class="link-article"></a>
+                      <div class="article-title">
+                        Nights of Azure Game RPG Dengan Bumbu "Yuri" Di Dalamnya
+                      </div>
+                      <div class="article-date">
+                        <span>20 OKTOBER 2017</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="article-wrapper">
+                    <a href="nights-of-azure" class="link-article"></a>
+                    <div class="img-article">
+                      <img src="{{ asset('assets/images/content/nights-of-azure.jpg') }}">
+                    </div>
+                    <div class="meta">
+                      <a href="#" class="link-article"></a>
+                      <div class="article-title">
+                        Nights of Azure Game RPG Dengan Bumbu "Yuri" Di Dalamnya
+                      </div>
+                      <div class="article-date">
+                        <span>20 OKTOBER 2017</span>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class="col s12 m6 l4">
+                  <div class="sidebar-title">
+                    <a href="#" class="more-button">More Game</a>
+                  </div>
+                  <div class="sidebar-content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+<footer>
+  <a href="#" class="copyright">COPYRIGHTS 2017 - 2021 © RIKATSU. ALL RIGHTS RESERVED</a>
+</footer>
+<script type="text/javascript" src="{{ asset('assets/js/jquery-3.2.1.min.js') }}"></script>
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('li a[data-toggle="search-dropdown"]').click(function(){
+      $('li#nav-search').toggleClass('active');
+      if ($('.search-dropdown').css('display') == 'block') {
+          $('.search-dropdown').css('display', 'none');
+      }else if($('.search-dropdown').css('display') == 'none'){
+          $('.search-dropdown').css('display', 'block');
+      }
+    });
+  });
+
+  $("img").on('error', function(){
+        $(this).hide();
+    });
+</script>
+</body>
+</html>
